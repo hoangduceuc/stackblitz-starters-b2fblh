c017b073417bb579cd475020c387fabd2d28ace4
hoangduceuc <113078626+hoangduceuc@users.noreply.github.com>
2024-03-27 09:25:58 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-b2fblh
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/hoangduceuc/stackblitz-starters-b2fblh)
---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Hello!');
+angular
+  .module('ungdungdautien', [])
+  .controller('ViDuController', function ($scope) {
+    $scope.xinchao = {};
+    $scope.xinchao.tieude = 'AngularJS';
+  });
